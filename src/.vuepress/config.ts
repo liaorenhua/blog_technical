ad79184acc37ebed75b1d696e11ad3e08a206642
liao_rh <993929808@qq.com>
2024-12-21 15:00:23 +0800

    1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/blog_technical",
   pagePatterns: ['**/*.md', '!.vuepress', '!node_modules', '!components'],
   head: [
     ['script', {
