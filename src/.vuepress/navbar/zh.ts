286d4ab40cf39f3b8f7a3e98480e5471710dad12
liao_rh <993929808@qq.com>
2026-02-07 16:15:59 +0800

    格式化

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -1,133 +1,133 @@
-import { navbar } from "vuepress-theme-hope";
+import {navbar} from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-  "/",
+    "/",
 
-  // 廖仁华 自定义新增导航条菜单
-  {
-    text: "数据库",
-    icon: "lightbulb",
-    /**
-     *  这里prefix值指向的是sidebar目录下里面的*.ts里面的key; 如果是英文的话类似: /en/guide/ 配置;
-     *  如果能指的到到时候显示左边会有菜单，否则没有菜单的
-     */
-    prefix: "/database/",
-    children: [
-      // {
-      //   // text: "概述",
-      //   icon: "lightbulb",
-      //   prefix: "01/",  // database 目录的下一层: 01
-      //   children: [
-      //       // link: "" 这个链接为空导致左边菜单 不会因为这个子菜单有任何改变，他只会引用: database对应的无序菜单
-      //     { text: "概述", icon: "ellipsis", link: "" }
-      //   ],
-      // },
-      {
-        text: "关系数据库 ",
+    // 廖仁华 自定义新增导航条菜单
+    {
+        text: "数据库",
         icon: "lightbulb",
-        // prefix: "02/",
+        /**
+         *  这里prefix值指向的是sidebar目录下里面的*.ts里面的key; 如果是英文的话类似: /en/guide/ 配置;
+         *  如果能指的到到时候显示左边会有菜单，否则没有菜单的
+         */
+        prefix: "/database/",
         children: [
-          { text: "Mysql", icon: "ellipsis", link: "01/" },
-          { text: "Redis", icon: "ellipsis", link: "02/" },
-          { text: "extend", icon: "ellipsis", link: "03/" },
-          { text: "DataX", icon: "ellipsis", link: "04/" },
+            // {
+            //   // text: "概述",
+            //   icon: "lightbulb",
+            //   prefix: "01/",  // database 目录的下一层: 01
+            //   children: [
+            //       // link: "" 这个链接为空导致左边菜单 不会因为这个子菜单有任何改变，他只会引用: database对应的无序菜单
+            //     { text: "概述", icon: "ellipsis", link: "" }
+            //   ],
+            // },
+            {
+                text: "关系数据库 ",
+                icon: "lightbulb",
+                // prefix: "02/",
+                children: [
+                    {text: "Mysql", icon: "ellipsis", link: "01/"},
+                    {text: "Redis", icon: "ellipsis", link: "02/"},
+                    {text: "extend", icon: "ellipsis", link: "03/"},
+                    {text: "DataX", icon: "ellipsis", link: "04/"},
+                ],
+            },
+            // {
+            //   // text: "mysql",
+            //   icon: "lightbulb",
+            //   prefix: "03/",
+            //   children: [
+            //     { text: "Mysql", icon: "ellipsis", link: "" }
+            //   ],
+            // },
         ],
-      },
-      // {
-      //   // text: "mysql",
-      //   icon: "lightbulb",
-      //   prefix: "03/",
-      //   children: [
-      //     { text: "Mysql", icon: "ellipsis", link: "" }
-      //   ],
-      // },
-    ],
-  },
-  "/monitor-tuning/",
-  "/http-protocol/",
-  // 架构  如此样式，会左边菜单不会更改的，所以子菜单共用一个左边一个菜单
-  // {
-  //   text: "架构",
-  //   icon: "circle-down",
-  //   prefix: "/framework/",
-  //   children: [
-  //     {
-  //       text: "概述",
-  //       icon: "lightbulb",
-  //       prefix: "01/",  // database 目录的下一层: 01
-  //       children: [
-  //         { text: "概述", icon: "ellipsis", link: "/code/", activeMatch: "^/code/$"}
-  //       ],
-  //     },
-  //     {
-  //       text: "redis",
-  //       icon: "lightbulb",
-  //       prefix: "02/",
-  //       children: [
-  //         { text: "DDD", icon: "ellipsis", link: "" }
-  //       ],
-  //     },
-  //   ],
-  // },
+    },
+    "/monitor-tuning/",
+    "/http-protocol/",
+    // 架构  如此样式，会左边菜单不会更改的，所以子菜单共用一个左边一个菜单
+    // {
+    //   text: "架构",
+    //   icon: "circle-down",
+    //   prefix: "/framework/",
+    //   children: [
+    //     {
+    //       text: "概述",
+    //       icon: "lightbulb",
+    //       prefix: "01/",  // database 目录的下一层: 01
+    //       children: [
+    //         { text: "概述", icon: "ellipsis", link: "/code/", activeMatch: "^/code/$"}
+    //       ],
+    //     },
+    //     {
+    //       text: "redis",
+    //       icon: "lightbulb",
+    //       prefix: "02/",
+    //       children: [
+    //         { text: "DDD", icon: "ellipsis", link: "" }
+    //       ],
+    //     },
+    //   ],
+    // },
     // 这个样式是 左边菜单随着导航条变动
-  {
-    text: "系统架构",
-    icon: "code",
-    children: [
-      {
-        text: "笔记",
+    {
+        text: "系统架构",
         icon: "code",
-        children: ["/ddd/", "/linux/"],
-      },
-      // {
-      //   // text: "产品设计",
-      //   children: ["/design/", "/linux/"],
-      // },
-      // {
-      //   // text: "后端运维",
-      //   children: ["/linux/"],
-      // },
-    ],
-  },
+        children: [
+            {
+                text: "笔记",
+                icon: "code",
+                children: ["/ddd/", "/linux/"],
+            },
+            // {
+            //   // text: "产品设计",
+            //   children: ["/design/", "/linux/"],
+            // },
+            // {
+            //   // text: "后端运维",
+            //   children: ["/linux/"],
+            // },
+        ],
+    },
 
 
-  // 下载链接
-  // "/download/",
+    // 下载链接
+    // "/download/",
     // 在线图鉴
-  // "/almanac/",
-  "/guide/",
-  // 自定义关卡
-  // "/custom-level/",
-  // 协议和声明
-  // "/instructions/",
+    // "/almanac/",
+    "/guide/",
+    // 自定义关卡
+    // "/custom-level/",
+    // 协议和声明
+    // "/instructions/",
 
-  // "/contribution/",
-  // "/devtool/",
-  // { text: "在线游玩", link: "https://play.pvzge.com", icon: "circle-play" },
+    // "/contribution/",
+    // "/devtool/",
+    // { text: "在线游玩", link: "https://play.pvzge.com", icon: "circle-play" },
 
-  // "/demo/",
-  // {
-  //   text: "指南",
-  //   icon: "lightbulb",
-  //   prefix: "/zh/guide/",
-  //   children: [
-  //     {
-  //       text: "Bar",
-  //       icon: "lightbulb",
-  //       prefix: "bar/",
-  //       children: ["baz", { text: "...", icon: "ellipsis", link: "" }],
-  //     },
-  //     {
-  //       text: "Foo",
-  //       icon: "lightbulb",
-  //       prefix: "foo/",
-  //       children: ["ray", { text: "...", icon: "ellipsis", link: "" }],
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: "V2 文档",
-  //   icon: "book",
-  //   link: "https://theme-hope.vuejs.press/zh/",
-  // },
+    // "/demo/",
+    // {
+    //   text: "指南",
+    //   icon: "lightbulb",
+    //   prefix: "/zh/guide/",
+    //   children: [
+    //     {
+    //       text: "Bar",
+    //       icon: "lightbulb",
+    //       prefix: "bar/",
+    //       children: ["baz", { text: "...", icon: "ellipsis", link: "" }],
+    //     },
+    //     {
+    //       text: "Foo",
+    //       icon: "lightbulb",
+    //       prefix: "foo/",
+    //       children: ["ray", { text: "...", icon: "ellipsis", link: "" }],
+    //     },
+    //   ],
+    // },
+    // {
+    //   text: "V2 文档",
+    //   icon: "book",
+    //   link: "https://theme-hope.vuejs.press/zh/",
+    // },
 ]);
--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,47 +1,47 @@
-import { sidebar } from "vuepress-theme-hope";
+import {sidebar} from "vuepress-theme-hope";
 import {linux} from "./linux.js";
 import {ddd} from "./ddd.js";
 
 export const zhSidebar = sidebar({
-  // 廖仁华，若这里不配置，则又导航条触发显示页面，左边没有菜单的; 用structure导航下面所有子菜单左边目录都一样
-  "/database/": "structure",
-  "/monitor-tuning/": "structure",
-  "/http-protocol/": "structure",
+    // 廖仁华，若这里不配置，则又导航条触发显示页面，左边没有菜单的; 用structure导航下面所有子菜单左边目录都一样
+    "/database/": "structure",
+    "/monitor-tuning/": "structure",
+    "/http-protocol/": "structure",
 
-  // 代码笔记导航条 framework 下面的子菜单
-  "/linux/": linux,
-  "/ddd/": ddd,
+    // 代码笔记导航条 framework 下面的子菜单
+    "/linux/": linux,
+    "/ddd/": ddd,
 
-  // 下载链接
-  // "/download/": "structure",
-  "/instructions/": "structure",
-  "/guide/": "structure",
-  // 协议和声明
-  // "/custom-level/": "structure",
-  // 用户社区
-  // "/contribution/": "structure",
-  // "/devtool/": "structure",
-  // "/almanac/": "structure",
+    // 下载链接
+    // "/download/": "structure",
+    "/instructions/": "structure",
+    "/guide/": "structure",
+    // 协议和声明
+    // "/custom-level/": "structure",
+    // 用户社区
+    // "/contribution/": "structure",
+    // "/devtool/": "structure",
+    // "/almanac/": "structure",
 
-  "/": [
-    // "portfolio",
-    // {
-    //   text: "案例",
-    //   icon: "laptop-code",
-    //   prefix: "demo/",
-    //   link: "demo/",
-    //   children: "structure",
-    // },
-    // {
-    //   text: "文档",
-    //   icon: "book",
-    //   prefix: "guide/",
-    //   children: "structure",
-    // },
-    // {
-    //   text: "幻灯片",
-    //   icon: "person-chalkboard",
-    //   link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
-    // },
-  ],
+    "/": [
+        // "portfolio",
+        // {
+        //   text: "案例",
+        //   icon: "laptop-code",
+        //   prefix: "demo/",
+        //   link: "demo/",
+        //   children: "structure",
+        // },
+        // {
+        //   text: "文档",
+        //   icon: "book",
+        //   prefix: "guide/",
+        //   children: "structure",
+        // },
+        // {
+        //   text: "幻灯片",
+        //   icon: "person-chalkboard",
+        //   link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
+        // },
+    ],
 });
